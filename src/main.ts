04e9f918fdd5908e8f92ec7b84d84621812a3c53
Viacheslav Hirshev <viacheslavgirshev@gmail.com>
2026-02-07 13:16:46 +0200

    feat(superheroes): added db connection and scripts for migrations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = await app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT');
+
+  await app.listen(port ?? 3000);
 }
 bootstrap();
